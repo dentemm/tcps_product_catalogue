3e66cba9ba078c235df40e3741f8932e01175bd8
Tim Claes <tim.claes@me.com>
2015-09-13 13:14:50 +0200

    Further implementation of supplier overview page

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     #url(r'^(?P<slug>[^/]+)/$', CategoryCreateView.as_view(), name='category_detail'),
 
     url(r'^overview/$', views.TestProductOverviewPage.as_view(), name='product-overview'),
+    url(r'^overview/suppliers/$', views.SupplierOverviewPage.as_view(), name='supplier-product-overview'),
     url(r'^category/(?P<category>[\w-]+)/$', views.CategorySubCategoryListView.as_view(), name='category-subcategory-list'),
     url(r'^suppliers/(?P<supplier>[\w-]+)/$', views.SupplierProductListView.as_view(), name='supplier-product-list'),
     url(r'^subcategory/(?P<subcategory>[\w-]+)/$', views.SubcategoryProductListView.as_view(), name='subcategory-product-list'),
