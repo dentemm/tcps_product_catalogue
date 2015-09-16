33bb4ec5d8e7fb1bbf79734f724fd7f4b04be3b8
Tim Claes <tim.claes@me.com>
2015-09-16 22:10:31 +0200

    Non minified version of tagsort

--- catalogue/static/tagsort/tagsort_edit.js
+++ catalogue/static/tagsort/tagsort_edit.js
@@ -1,106 +1,119 @@
-!function(e){
-	e.fn.tagSort=function(t){ 
-	  // Deze regel defineert de tagSort() functie als een jQuery functie. Hier mee kan je dus $.tagSort() oproepen in je eigen scripts
-	  
-		var a={ // a: init params
+;(function($) {
+  $.fn.tagSort = function(options) {
 
-			selector:".item-tagsort", // default waarde voor de css klasse die een tagsort item aangeeft
-			tagWrapper:"span", // tags will be wrapped in <span> elements
-			displaySelector:!1, // false
-			displaySeperator:" ", // seperator voor tags?
-			inclusive:!1, // false
-			fadeTime:200
-		};
+      var defaults = { // Default settings
+        selector: '.item-tagsort',
+        tagWrapper: 'span',
+        displaySelector: false,
+        displaySeperator: ' ',
+        inclusive: false, 
+        fadeTime: 200
+      };
 
-		t=e.extend(a,t); // jQuery extend() function: merge the content of two or more objects together into the first object
+      options = $.extend(defaults, options); // Gebruik de defaults als er geen options zijn 
 
-		var n={
-			generateTags:function(a){ // Deze funtie is verantwoordelijk voor het automatisch aanmaken van de tags
-				
-				var i={},
+      var tagSortEngine = { // De engine
 
-				s={ //dictionary
-					elements:[],
-					tags:[]
-				},
+        generateTags: function(elements) { // Generate Tags
+          
+          var tags_inclusive = {};
+          var tags_exclusive = {elements: [], tags: []};
 
-				r=e(document.createElement(t.tagWrapper)); // JavaScript createElement function, dit maakt een <span> element aan
+          var tagElement = $(document.createElement(options.tagWrapper)); // Maak een <span> element aan
 
-				return a.each(function(){ // jQuery each() function: itereer over jQuery object, waarbij de functie wordt uitgevoerd op elk element dat voldoet
+          elements.each(function(i){ // Voor elk element:
 
-					$element=e(this); // $element is het door jQuery geselecteerde element
+            $element = $(this);
 
-					var a=$element.data("item-tags"), // Ga op zoek naar het html data attribuut 'data-item-tags'
+            var tagsData = $element.data('item-tags'), // Selecteer de tags, via het data attribute 'data-item-tags'
+            elementTags = tagsData.match(/,\s+/) ? tagsData.split(', ') : tagsData.split(','); // De tags zelf zijn gescheiden door komma's
+            
+            $.each(elementTags, function(i, v){ 
 
-					c=a.split(a.match(/,\s+/)?", ":","); // Split de waarde voor data-item-tags op volgens ':' of ','
+              var tagName = v.toLowerCase();
 
-					e.each(c,function(e,a){ // Voor elk van de tags uit de waarde van 'data-item-tags':
-						var s=a.toLowerCase(); // Maak lowercase
+              if(!tags_inclusive[tagName]){
+                tags_inclusive[tagName] = [];
+                tagSortEngine.container.append(tagElement.clone().text(v));
+              }
 
-						i[s]||(i[s]=[],n.container.append(r.clone().text(a))),
-						t.displaySelector!==!1&&$element.find(t.displaySelector).append(e>0?t.displaySeperator+a:a),
-						i[s].push($element)
-					}),
-					s.elements.push($element), // Voeg het element toe aan de dict
-					s.tags.push(c) //Voeg de tag toe aan de dict
-				}),
-				1==t.inclusive?i:s // Check de inclusive parameter
-			}, 
+              if(options.displaySelector !== false){
+                $element.find(options.displaySelector).append(i > 0 ? options.displaySeperator + v : v);
+              }
 
-			exclusiveSort:function(t){ // Functie die wordt gebruik in geval van niet inclusive sort
+              tags_inclusive[tagName].push($element);
+            });
 
-				var a=[[],[]];
+            tags_exclusive.elements.push($element);
+            tags_exclusive.tags.push(elementTags);
+          });
+          return options.inclusive == true ? tags_inclusive:tags_exclusive;
+        },
 
-				return e.each(t.elements,function(i,s){
-					var r=!0;n.container.find(".tagsort-active").each(function(){
-						-1==t.tags[i].indexOf(e(this).text())&&(r=!1,a[0].push(s))
-					}),
-					1==r&&a[1].push(s)
-				}), a
-			},
+        exclusiveSort: function(tags, elements){
+          var display = [[],[]];
+          $.each(tags.elements, function(element_key, element){
+            var showElement = true;
+            tagSortEngine.container.find('.tagsort-active').each(function(i){
+              if(tags.tags[element_key].indexOf($(this).text()) == -1){
+                showElement = false;
+                display[0].push(element);
+              }
+            });
 
-			inclusiveSort:function(t){ // Functie die wordt gebruikt in geval van inclusive sort
-				var a=[[],[]];
-				return n.container.find(".tagsort-active").each(function(){
-					e.each(t[e(this).text().toLowerCase()],function(e,t){
-						a[1].push(t)
-					})
-				}), a
-			},
+            if(showElement == true) {
+              display[1].push(element);
+            }
+          });
+          return display;
+        },
 
-			inititalize:function(a){
+        inclusiveSort: function(tags, elements){
+          var display = [[],[]]
+          tagSortEngine.container.find('.tagsort-active').each(function(i){
+            $.each(tags[$(this).text().toLowerCase()],function(element_key, element){
+              display[1].push(element);
 
-				n.container=a, 
-				n.container.addClass("tagsort-tags-container"); 
+            });
+          });
+          return display;
+        },
 
-				var i=e(t.selector); // De selector is de klasse die aan de items werd toegevoegd
+        inititalize: function(tagsContainer){
+          
+          tagSortEngine.container = tagsContainer;
+          tagSortEngine.container.addClass('tagsort-tags-container');
+          var elements = $(options.selector);
+          tagSortEngine.tags = tagSortEngine.generateTags(elements, tagSortEngine.container);
+          var tagElement = tagSortEngine.container.find(options.tagWrapper);
 
-				n.tags=n.generateTags(i,n.container); // 
-
-				var s=n.container.find(t.tagWrapper); // 
-
-				s.click(function(){
-
-					if(e(this).toggleClass("tagsort-active"), s.hasClass("tagsort-active")){
-
-						i.fadeOut(t.fadeTime); // jQuery fadeOut() functie: deze verbergt de element door ze te uit faden met de fadeTime parameter
-
-						var a=1==t.inclusive?n.inclusiveSort(n.tags,i):n.exclusiveSort(n.tags,i);
-
-						a[0].length>0&&e.each(a[0],function(e,a){
-
-							a.is(":visible")&&a.fadeOut(t.fadeTime)
-
-						}),a[1].length>0&&e.each(a[1],function(e,a){
-
-							a.is("visible")||a.fadeIn(t.fadeTime)
-						})
-					} else i.fadeIn(t.fadeTime) // jQuery fadeIn() functie: omgekeerde van fadeOut()
-				})
-			}
-		};
-		return n.inititalize(this),e(this) // Initialiseer tagSort
-	}
-}(jQuery);
-
-
+          tagElement.click(function(){
+            $(this).toggleClass('tagsort-active');
+            if(!tagElement.hasClass('tagsort-active')){
+              elements.fadeIn(options.fadeTime);
+            }
+            else {
+              elements.fadeOut(options.fadeTime);
+              var display = options.inclusive == true ? tagSortEngine.inclusiveSort(tagSortEngine.tags, elements):tagSortEngine.exclusiveSort(tagSortEngine.tags, elements);
+              if(display[0].length > 0){
+                $.each(display[0], function(hide_key, toHide){
+                  if(toHide.is(':visible')){
+                    toHide.fadeOut(options.fadeTime);
+                  }
+                });
+              }
+              if(display[1].length > 0){
+                $.each(display[1], function(hide_key, toShow){
+                  if(!toShow.is('visible')){
+                    toShow.fadeIn(options.fadeTime);
+                  }
+                });
+              }
+            }
+          });
+        }
+      }
+      tagSortEngine.inititalize(this);
+      return $(this);
+    }
+})(jQuery);
