fc532df2328ef732bca08a8a45f0b7b965031793
Tim Claes <tim.claes@me.com>
2015-09-16 18:48:17 +0200

    continued tagsort analysis

--- catalogue/static/tagsort/tagsort_edit.js
+++ catalogue/static/tagsort/tagsort_edit.js
@@ -1,7 +1,9 @@
 !function(e){
 	e.fn.tagSort=function(t){ 
 	  // Deze regel defineert de tagSort() functie als een jQuery functie. Hier mee kan je dus $.tagSort() oproepen in je eigen scripts
-		var a={
+	  
+		var a={ // a: init params
+
 			selector:".item-tagsort", // default waarde voor de css klasse die een tagsort item aangeeft
 			tagWrapper:"span", // tags will be wrapped in <span> elements
 			displaySelector:!1, // false
@@ -39,14 +41,16 @@
 						t.displaySelector!==!1&&$element.find(t.displaySelector).append(e>0?t.displaySeperator+a:a),
 						i[s].push($element)
 					}),
-					s.elements.push($element), 
-					s.tags.push(c)
+					s.elements.push($element), // Voeg het element toe aan de dict
+					s.tags.push(c) //Voeg de tag toe aan de dict
 				}),
 				1==t.inclusive?i:s // Check de inclusive parameter
 			}, 
 
 			exclusiveSort:function(t){ // Functie die wordt gebruik in geval van niet inclusive sort
+
 				var a=[[],[]];
+
 				return e.each(t.elements,function(i,s){
 					var r=!0;n.container.find(".tagsort-active").each(function(){
 						-1==t.tags[i].indexOf(e(this).text())&&(r=!1,a[0].push(s))
@@ -61,29 +65,41 @@
 					e.each(t[e(this).text().toLowerCase()],function(e,t){
 						a[1].push(t)
 					})
-				}),a
+				}), a
 			},
 
 			inititalize:function(a){
-				n.container=a,
-				n.container.addClass("tagsort-tags-container");
-				var i=e(t.selector);
-				n.tags=n.generateTags(i,n.container);
-				var s=n.container.find(t.tagWrapper);
+
+				n.container=a, 
+				n.container.addClass("tagsort-tags-container"); 
+
+				var i=e(t.selector); // De selector is de klasse die aan de items werd toegevoegd
+
+				n.tags=n.generateTags(i,n.container); // 
+
+				var s=n.container.find(t.tagWrapper); // 
+
 				s.click(function(){
+
 					if(e(this).toggleClass("tagsort-active"), s.hasClass("tagsort-active")){
-						i.fadeOut(t.fadeTime);
+
+						i.fadeOut(t.fadeTime); // jQuery fadeOut() functie: deze verbergt de element door ze te uit faden met de fadeTime parameter
+
 						var a=1==t.inclusive?n.inclusiveSort(n.tags,i):n.exclusiveSort(n.tags,i);
+
 						a[0].length>0&&e.each(a[0],function(e,a){
+
 							a.is(":visible")&&a.fadeOut(t.fadeTime)
+
 						}),a[1].length>0&&e.each(a[1],function(e,a){
+
 							a.is("visible")||a.fadeIn(t.fadeTime)
 						})
-					} else i.fadeIn(t.fadeTime)
+					} else i.fadeIn(t.fadeTime) // jQuery fadeIn() functie: omgekeerde van fadeOut()
 				})
 			}
 		};
-		return n.inititalize(this),e(this)
+		return n.inititalize(this),e(this) // Initialiseer tagSort
 	}
 }(jQuery);
 
