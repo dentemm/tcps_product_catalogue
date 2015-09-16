db033459681dcaf57d24eca90ddfa6862dc742fd
Tim Claes <tim.claes@me.com>
2015-09-16 17:46:25 +0200

    Added comments to tagsort css and js

--- catalogue/static/tagsort/tagsort_edit.js
+++ catalogue/static/tagsort/tagsort_edit.js
@@ -1,59 +1,70 @@
 !function(e){
-	e.fn.tagSort=function(t){
+	e.fn.tagSort=function(t){ 
+	  // Deze regel defineert de tagSort() functie als een jQuery functie. Hier mee kan je dus $.tagSort() oproepen in je eigen scripts
 		var a={
-			selector:".item-tagsort",
-			tagWrapper:"span",
-			displaySelector:!1,
-			displaySeperator:" ",
-			inclusive:!1,
+			selector:".item-tagsort", // default waarde voor de css klasse die een tagsort item aangeeft
+			tagWrapper:"span", // tags will be wrapped in <span> elements
+			displaySelector:!1, // false
+			displaySeperator:" ", // seperator voor tags?
+			inclusive:!1, // false
 			fadeTime:200
 		};
-		t=e.extend(a,t);
+
+		t=e.extend(a,t); // jQuery extend() function: merge the content of two or more objects together into the first object
+
 		var n={
-			generateTags:function(a){
+			generateTags:function(a){ // Deze funtie is verantwoordelijk voor het automatisch aanmaken van de tags
+				
 				var i={},
-				s={
+
+				s={ //dictionary
 					elements:[],
 					tags:[]
 				},
-				r=e(document.createElement(t.tagWrapper));
 
-				return a.each(function(){
+				r=e(document.createElement(t.tagWrapper)); // JavaScript createElement function, dit maakt een <span> element aan
 
-					$element=e(this);
+				return a.each(function(){ // jQuery each() function: itereer over jQuery object, waarbij de functie wordt uitgevoerd op elk element dat voldoet
 
-					var a=$element.data("item-tags"), 
-					c=a.split(a.match(/,\s+/)?", ":",");
+					$element=e(this); // $element is het door jQuery geselecteerde element
 
-					e.each(c,function(e,a){
-						var s=a.toLowerCase();
+					var a=$element.data("item-tags"), // Ga op zoek naar het html data attribuut 'data-item-tags'
 
-						i[s]||(i[s]=[],
-							n.container.append(r.clone().text(a))),
-							t.displaySelector!==!1&&$element.find(t.displaySelector).append(e>0?t.displaySeperator+a:a),
-							i[s].push($element)
-						}),
-					s.elements.push($element),
-					s.tags.push(c)}),
-				1==t.inclusive?i:s
+					c=a.split(a.match(/,\s+/)?", ":","); // Split de waarde voor data-item-tags op volgens ':' of ','
+
+					e.each(c,function(e,a){ // Voor elk van de tags uit de waarde van 'data-item-tags':
+						var s=a.toLowerCase(); // Maak lowercase
+
+						i[s]||(i[s]=[],n.container.append(r.clone().text(a))),
+						t.displaySelector!==!1&&$element.find(t.displaySelector).append(e>0?t.displaySeperator+a:a),
+						i[s].push($element)
+					}),
+					s.elements.push($element), 
+					s.tags.push(c)
+				}),
+				1==t.inclusive?i:s // Check de inclusive parameter
 			}, 
-			exclusiveSort:function(t){
+
+			exclusiveSort:function(t){ // Functie die wordt gebruik in geval van niet inclusive sort
 				var a=[[],[]];
 				return e.each(t.elements,function(i,s){
 					var r=!0;n.container.find(".tagsort-active").each(function(){
 						-1==t.tags[i].indexOf(e(this).text())&&(r=!1,a[0].push(s))
 					}),
 					1==r&&a[1].push(s)
-				}),a
+				}), a
 			},
-			inclusiveSort:function(t){
+
+			inclusiveSort:function(t){ // Functie die wordt gebruikt in geval van inclusive sort
 				var a=[[],[]];
 				return n.container.find(".tagsort-active").each(function(){
 					e.each(t[e(this).text().toLowerCase()],function(e,t){
 						a[1].push(t)
 					})
 				}),a
-			},inititalize:function(a){
+			},
+
+			inititalize:function(a){
 				n.container=a,
 				n.container.addClass("tagsort-tags-container");
 				var i=e(t.selector);
@@ -75,21 +86,5 @@
 		return n.inititalize(this),e(this)
 	}
 }(jQuery);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
